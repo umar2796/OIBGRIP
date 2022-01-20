7db9584a79adcd861ed1fd62a6738d3f7078ca57
umar2796 <97377612+umar2796@users.noreply.github.com>
2022-01-20 12:42:09 +0530

    Add files via upload

---
+++ Calculator/public_html/calc_javascript.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// This function clear all the values
+function clearScreen() {
+ document.getElementById("result").value = "";
+}
+
+// This function display values
+function display(value) {
+ document.getElementById("result").value += value;
+}
+// This function evaluates the expression and return result
+function calculate() {
+ var p = document.getElementById("result").value;
+ var q = eval(p);
+ document.getElementById("result").value = q;
+}
+
